0da4b0efd86797b44249307c15d3d2a6b6f3525c
Willie Jeng <willie.jeng@gmail.com>
2018-09-19 19:34:17 -0700

    First commit woohoo

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # CompProgBot
-Discord bot for competitive programming. 
+A Discord bot for competitive programming. 
+
+## About
+
+### History 
+I wrote a Discord bot called oj-linker for my Taiwanese informatics Discord server in 2017. This is basically a copy of that but in English and minus all the inside jokes (and a bunch of really cool features no one would ever use). It even had automatic reminders before every Codeforces round, but apparently Americans don't (have the correct timezone to) do Codeforces. You can still find it in my repos.
+
+I learned how to make a Discord bot by watching this tutorial: [https://www.youtube.com/watch?v=-5jJaO17Gjs](https://www.youtube.com/watch?v=-5jJaO17Gjs). 
+
+### Language
+This bot was written in JavaScript (node.js), and I use [discord.js](https://discord.js.org/) to interact with the Discord API.
+
+### Server
+I currently just run this bot on my Asus laptop at home, so it might be quite slow and the Internet connection might cut out every once in a while (I know it's an annoyance). I used to use Heroku to host oj-linker, but I would run out of free hours for the last week of each month and so my bot would only be online for three weeks every month. I might switch back to using Heroku if my laptop is too slow (or if I decide I want to turn of my laptop).
+
+TL;DR: Currently using laptop as server, might use Heroku in the future.
+
+## Commands
+All commands start with the `~` prefix.
+
+### Bot info
+`~help` or `~about`: Shows a help message.
+
+`~commands`: Lists commands (what it really does is link to this page).
+
+`~bug`: Report a bug. More specifically, link to the issues page.
+
+### Online judges
+`~uva [id]`: Links to and shows information of a certain UVa Online Judge problem.
+
+### Other
+none yet :(
+
+### Might add in future
+- Online judges to support:
+    - Codeforces
+    - Codechef
+    - and others
+- Reddit (something like top post of r/ProgrammerHumor)
+- Any other suggestions
---
+++ index.js
@@ -0,0 +1,109 @@
+// you can tell that im really cool because i write all my code in one single file
+
+const Discord = require('discord.js');
+const request = require('request');
+
+const TOKEN = 'NDkyMTMzMzM1MTMzNTg1NDE4.DoSAGg.gtM_C5z5wkSddYzFNSsBDLug2cE';
+
+var isNum = function(c){
+	for(j=0; j<10; j++)
+		if(c == j) return true;
+	return false;
+}
+
+
+var bot = new Discord.Client();
+var PREFIX = '~';
+
+bot.on("ready", function(){
+	console.log('CompProgBot is ready for use!'); // log to console
+	bot.user.setPresence({ 
+		game: { // set game
+			name: PREFIX + 'help',
+			type: 0
+		}
+	});
+});
+
+bot.on('message', function(message){
+
+	if(message.author.bot) return; // ignore bot messages
+
+	if(message.isMentioned(bot.user)){
+		message.channel.send("Please use the prefix '" + PREFIX + "' to use commands. Ex: ~help");
+		return;
+	}
+
+	if(!message.content.startsWith(PREFIX)) return; // ignore messages without prefix
+
+	console.log("Received " + message.author.username + "'s command: " + message.content.substring(0,40)); // log to console
+
+	var args = message.content.substring(PREFIX.length).split(" ");
+	switch(args[0].toLowerCase()){ // not case sensitive
+
+		//===== BOT INFO =====
+
+		case 'help':
+		case 'about':
+			message.channel.send(
+				"Beep boop I'm a bot\n" + 
+				"My commands start with the prefix '" + PREFIX + "'\n" +
+				'For commands, type "' + PREFIX + 'commands"\n' +
+				"My source code is available at https://github.com/wj3ng/CompProgBot"
+			);
+			return;
+
+		case 'commands':
+			message.channel.send(
+				"For a complete list of commands, please go to https://github.com/wj3ng/CompProgBot/blob/master/README.md"
+			);
+			return;
+
+		case 'bug':
+			message.channel.send(
+				"To report a bug, please go to https://github.com/wj3ng/CompProgBot/issues"
+			);
+			return;
+
+		//===== ONLINE JUDGES =====
+
+		case 'uva':
+			
+			let uvaValID = true;
+			if(args[1].length > 5) uvaValID = false;
+			for(i=0; i<args[1].length; ++i)
+				if(!isNum(args[1].charAt(i))) uvaValID = false;
+
+			if(!args[1]) message.channel.send('Please state a problem number. Ex: "~uva 00100"');
+			else if(!uvaValID) message.channel.send('Invalid format. Ex: "~uva 00100"');
+			else{
+				request("https://uhunt.onlinejudge.org/api/p/num/"+args[1], {json: true}, function(err, resp, body){
+					if(body.num == undefined) message.channel.send("Problem does not exist.");
+					else{
+						var uvaEmbed = new Discord.RichEmbed()
+							.addField("UVa " + body.num + ": " + body.title,
+								"https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=" + body.pid +
+								"\n" + body.dacu + " users accepted.")
+							.setFooter("AC " + body.ac + ", WA " + body.wa + ", TLE " + body.tle + ", MLE " + body.mle + ", RE " + body.re + ", PE " + body.pe + ", CE " + body.ce)
+							.setColor(0xA50E57)
+							message.channel.send(uvaEmbed);
+					}
+				});
+			}
+			return;
+
+		case 'cf':
+		case 'codeforces':
+			return;
+
+		//===== MISC =====
+
+
+
+	}
+
+
+});
+
+// start the bot
+bot.login(TOKEN);
